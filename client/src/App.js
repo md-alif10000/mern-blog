5c499a632a546b1ed6c16e94da27b90e3bb50189
md_alif <md.alif10000@gmail.com>
2021-04-30 21:42:13 +0600

    added-redux-store

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,23 @@ import Navbar from './components/Navbar';
 import Login from './containers/auth/Login';
 import Register from './containers/auth/Register';
 import Home from './containers/Home';
+import {Provider} from "react-redux";
 import './main.scss'
+import store from './store';
 
 
 function App() {
   return (
-		<Router>
-			<Navbar />
-			<Switch>
-				<Route path='/login' component={Login} />
-				<Route path='/register' component={Register} />
-				<Route path='/' exact component={Home} />
-			</Switch>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path='/login' component={Login} />
+					<Route path='/register' component={Register} />
+					<Route path='/' exact component={Home} />
+				</Switch>
+			</Router>
+		</Provider>
 	);
 }
 
---
+++ client/src/store/reducers/AuthReducer.js
@@ -0,0 +1,19 @@
+
+const initState={
+    loading:false,
+    registerErrors:[],
+    loginErrors:[]
+
+}
+
+const AuthReducer=(state=initState,action)=>{
+    switch(action.type){
+        
+        default:
+            return state
+    }
+
+}
+
+
+export default AuthReducer
