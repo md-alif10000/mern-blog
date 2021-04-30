705d3739a4b29a16fc7b0570ef7221594290ebb3
md_alif <md.alif10000@gmail.com>
2021-05-01 02:32:57 +0600

    saved token to localstorage

--- client/src/store/reducers/AuthReducer.js
+++ client/src/store/reducers/AuthReducer.js
@@ -1,10 +1,29 @@
 import { authTypes } from "../types";
+import jwt_decode from 'jwt-decode'
 
 const initState = {
 	loading: false,
 	registerErrors: [],
 	loginErrors: [],
+	token:'',
+	user:{}
 };
+const token=localStorage.getItem('token')
+if(token){
+	const decodedToken = jwt_decode(token);
+	const expireIn=new Date(decodedToken.exp * 1000)
+	console.log(expireIn);
+	if(new Date() > expireIn){
+		localStorage.removeItem('token')
+	}else{
+		const { user } = decodedToken;
+		initState.token=token
+		initState.user=user
+	}
+
+}
+
+
 
 const AuthReducer = (state = initState, action) => {
 	switch (action.type) {
